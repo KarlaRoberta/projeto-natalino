cfca18118b738440cafbab14da0e2b7cac0c3e61
KarlaR <karlarobertati@gmail.com>
2023-11-19 23:40:26 -0300

    efeito de neve caindo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+document.addEventListener('DOMContentLoaded', function () {
+    const numSnowflakes = 50;
+  
+    for (let i = 0; i < numSnowflakes; i++) {
+      createSnowflake();
+    }
+  
+    function createSnowflake() {
+      const snowflake = document.createElement('div');
+      snowflake.className = 'snowflake';
+      snowflake.style.left = Math.random() * 100 + 'vw';
+      snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
+      document.querySelector('.snowflakes').appendChild(snowflake);
+    }
+  });
